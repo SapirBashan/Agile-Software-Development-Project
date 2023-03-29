c0af6282e815afd58889857ee744eb326b2b34ea
yshsh <yshshapira@gmail.com>
2023-03-29 10:21:38 +0300

    Fixed the tests of Point and Vector

--- Unittest/primitives/PointTest.java
+++ Unittest/primitives/PointTest.java
@@ -1,8 +1,7 @@
 package primitives;
 
-import org.junit.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PointTest {
 
@@ -11,38 +10,35 @@ class PointTest {
      */
     @org.junit.jupiter.api.Test
     public void testAdd() {
+        Point p = new Point(1, 2, 3);
+
         // ============ Equivalence Partitions Tests ==============
 
         // TC01: two different points
-        Point p1 = new Point(1, 2, 3);
-        Vector p2 =  new Vector(2, 3, 4);
-        assertEquals(new Point(3, 5, 7), p1.add(p2), "ERROR: Point + Vector does not work correctly");
-
+        Vector v =  new Vector(2, 3, 4);
+        assertEquals(new Point(3, 5, 7), p.add(v), "ERROR: Point + Vector does not work correctly");
 
         // =============== Boundary Values Tests ==================
 
-        // Tc10 result is zero
-        Point p11 = new Point(1, 2, 3);
-        Vector p22 =  new Vector(-1, -2, -3);
-        assertEquals(new Point(0, 0, 0), p11.add(p22), "ERROR: Point + Vector does not work correctly");
-
+        // TC10: result is zero
+        assertEquals(new Point(0,0,0), p.add(new Vector(-1, -2, -3)));
     }
     /**
      * Test method for {@link primitives.Point#subtract(primitives.Point)}.
      */
     @org.junit.jupiter.api.Test
     void testSubtract() {
+        Point p1 = new Point(3, 4, 5);
+
         // ============ Equivalence Partitions Tests ==============
 
         // TC01: two different points
-        Point p1 = new Point(3, 4, 5);
         Point p2 =  new Point(2, 3, 4);
         assertEquals(new Vector(1, 1, 1), p1.subtract(p2), "ERROR: Point - Vector does not work correctly");
 
-
         // =============== Boundary Values Tests ==================
 
-        // Tc10 result is zero
+        // TC10: result is zero
         assertThrows(IllegalArgumentException.class, () -> p1.subtract(p1), "ERROR: Point - Vector does not work correctly");
     }
      /**
@@ -50,24 +46,24 @@ class PointTest {
      */
     @org.junit.jupiter.api.Test
     void distanceSquared() {
+
         // ============ Equivalence Partitions Tests ==============
         // TC01: two different points
         Point p1 = new Point(1, 2, 3);
         Point p2 =  new Point(2, 3, 4);
         assertEquals(3, p1.distanceSquared(p2), "ERROR: Point distanceSquared does not work correctly");
-
-
     }
+
     /**
      * Test method for {@link primitives.Point#distance(primitives.Point)}.
      */
     @org.junit.jupiter.api.Test
     void distance() {
+
         // ============ Equivalence Partitions Tests ==============
         // TC01: two different points
         Point p1 = new Point(1, 2, 3);
         Point p2 =  new Point(2, 3, 4);
         assertEquals(Math.sqrt(3), p1.distance(p2), "ERROR: Point distance does not work correctly");
-
     }
 }
--- Unittest/primitives/VectorTest.java
+++ Unittest/primitives/VectorTest.java
@@ -1,7 +1,6 @@
 package primitives;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class VectorTest {
@@ -13,83 +12,86 @@ class VectorTest {
     }
     @Test
     void testAdd() {
+        Vector v1 = new Vector(1, 2, 3);
 
         // ============ Equivalence Partitions Tests ==============
+
         // TC01: two different points that are not orthogonal
-        Vector v1 = new Vector(1, 2, 3);
         Vector v2 = new Vector(1, 4, 5);
         assertEquals(new Vector(2, 6, 8), v1.add(v2), "ERROR: Point + Vector does not work correctly");
 
-        // TC02 result is zero
-        assertThrows(IllegalArgumentException.class, () -> v1.add(new Vector(-1,-2,-3)), "ERROR: Point + Vector does not work correctly");
-
         // =============== Boundary Values Tests ==================
-        // TC03: the vector is orthogonal to the vector
-        Vector v4 = new Vector(-1,-2,-3);
-        assertThrows(IllegalArgumentException.class, () -> v1.add(v4), "ERROR: Point + Vector does not work correctly");
+
+        // TC10: result is zero
+        assertThrows(IllegalArgumentException.class, () -> v1.add(v1.scale(-1.0)), "ERROR: Point + Vector does not work correctly");
+
     }
 
     @Test
     void testScale() {
+        Vector v = new Vector(1, 2, 3);
+
         // ============ Equivalence Partitions Tests ==============
-        // TC04: two different points that are not ortogonal
-        Vector p1 = new Vector(1, 2, 3);
-        assertEquals(new Vector(2, 4, 6), p1.scale(2.0), "ERROR: Double * Vector does not work correctly");
+        // TC01: multiple by non zero number
+        assertEquals(new Vector(2, 4, 6), v.scale(2.0), "ERROR: Double * Vector does not work correctly");
 
         // =============== Boundary Values Tests ==================
-        // TC05 result is zero
-        assertThrows(IllegalArgumentException.class, () -> p1.scale(0.0), "ERROR: Double * Vector does not work correctly");
+        // TC10: result is zero
+        assertThrows(IllegalArgumentException.class, () -> v.scale(0.0), "ERROR: Double * Vector does not work correctly");
     }
 
     @Test
     void testCrossProduct() {
-        // ============ Equivalence Partitions Tests ==============
-        // TC06: two different points that are not ortogonal
         Vector v1 = new Vector(1, 2, 3);
+
+        // ============ Equivalence Partitions Tests ==============
+
+        // TC01: two different vectors that are not parallel
         Vector v2 = new Vector(2, 3, 4);
         assertEquals(new Vector(-1, 2, -1), v1.crossProduct(v2), "ERROR: crossProduct() for two different vectors does not work correctly");
 
-        // TC07: two vectors are orthogonal
-        assertThrows(IllegalArgumentException.class, () -> v1.crossProduct(v1.scale(-1.0)), "ERROR: Double * Vector does not work correctly");
+        // =============== Boundary Values Tests ==================
 
-
-
+        // TC10: two vectors are parallel
+        assertThrows(IllegalArgumentException.class, () -> v1.crossProduct(v1.scale(2.0)), "ERROR: Double * Vector does not work correctly");
     }
 
     @Test
     void lengthSquared() {
-       //this function is based on dot product and we already tested dot product
+       //this function is based on dot product, and we already tested dot product
     }
 
     @Test
     void length() {
-        //this function is based on dot product and we already tested dot product
+        //this function is based on dot product, and we already tested dot product
     }
 
 
     @Test
     void normalize() {
+
         // ============ Equivalence Partitions Tests ==============
-        // TC10: two different vectors that are not orthogonal
-        Vector v1 = new Vector(1, 2, 3);
-        Vector Normal = v1.normalize();
+
+        // TC01: normalize a vector
+        Vector v = new Vector(1, 2, 3);
+        Vector Normal = v.normalize();
         assertEquals(0, Normal.length() -1 , "ERROR: normalize() function creates a vector that is not normal to itself");
-
-
-
     }
 
 
     @Test
     void dotProduct() {
-        // ============ Equivalence Partitions Tests ==============
-        // TC08: two different points that are not ortogonal
         Vector v1 = new Vector(1, 2, 3);
+
+        // ============ Equivalence Partitions Tests ==============
+
+        // TC01: two different points that are not orthogonal
         Vector v2 = new Vector(2, 3, 4);
         assertEquals(20, v1.dotProduct(v2), "ERROR: dotProduct() for two different vectors does not work correctly");
 
         // =============== Boundary Values Tests ==================
-        // TC09: two vectors are orthogonal
+
+        // TC10: two vectors are orthogonal
         assertEquals(0, v1.dotProduct(new Vector(2,-1,0)), "ERROR: dotProduct() for two orthogonal vectors does not work correctly");
     }
 }
